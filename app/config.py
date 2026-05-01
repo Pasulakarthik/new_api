3599aa3bf1fcc3d45aeb792fe79d18c9e7b215d6
Karthik <pasulakarthik03@gmail.com>
2026-05-01 12:40:50 +0530

    Add files via upload

--- app/config.py
+++ app/config.py
@@ -1,7 +1,14 @@
 import os
 from fastapi.templating import Jinja2Templates
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+from jinja2 import Environment, FileSystemLoader
 
-templates = Jinja2Templates(
-    directory=os.path.join(BASE_DIR, "templates")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+template_dir = os.path.join(BASE_DIR, "templates")
+
+templates = Jinja2Templates(directory=template_dir)
+
+templates.env = Environment(
+    loader=FileSystemLoader(template_dir),
+    cache_size=0  
 )
